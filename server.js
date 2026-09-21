4cef3772e2bb10e3f80cd9578d71275f9fd47b90
sainiajay1726-code <sainiajay1726@gmail.com>
2026-09-21 16:03:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,16 +76,16 @@ async function githubRequest(endpoint, options = {}) {
     throw new Error('GitHub integration is not configured in Render environment variables.');
   }
 
-  const response = await fetch(https://api.github.com${endpoint}, {
-    ...options,
-    headers: {
-      Accept: 'application/vnd.github+json',
-      Authorization: Bearer ${GITHUB_TOKEN},
-      'X-GitHub-Api-Version': '2026-03-10',
-      'Content-Type': 'application/json',
-      ...(options.headers || {})
-    }
-  });
+  const response = await fetch(`https://api.github.com${endpoint}`, {
+  ...options,
+  headers: {
+    Accept: 'application/vnd.github+json',
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+    'X-GitHub-Api-Version': '2026-03-10',
+    'Content-Type': 'application/json',
+    ...(options.headers || {})
+  }
+});
 
   const text = await response.text();
 
